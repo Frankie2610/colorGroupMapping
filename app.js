2b660c71c22fba82842b614001b49d483cc73a30
Văn Gia Quyền <giaquyenvan@gmail.com>
2026-01-09 15:33:47 +0700

    fix pre-SKU

--- app.js
+++ app.js
@@ -76,12 +76,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
             // Xác định số ký tự để lấy làm prefix theo Vendor
             let prefixLength = 5; // mặc định
-            debugger;
             if (["VERSACE", "FERRAGAMO"].includes(vendor)) prefixLength = 4;
             else if (["PHILIPP PLEIN", "VERSUS BY VERSACE"].includes(vendor)) prefixLength = 5;
             else if (["TED BAKER", "MISSONI", "GUESS"].includes(vendor)) prefixLength = 6;
-            else if (vendor === "FURLA") prefixLength = 10;
+            else if (vendor === "ADIDAS") prefixLength = 7;
             else if (vendor === "LOCMAN") prefixLength = 8;
+            else if (vendor === "FURLA") prefixLength = 10;
 
             const skuPrefix = sku.substring(0, prefixLength);
             const optionId = skuPrefix;
